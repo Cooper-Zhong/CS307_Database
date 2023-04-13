79188e98df243c8343778b45d86e4806e34e9e7e
Cooper <12110517@mail.sustech.edu.cn>
2023-04-13 17:07:31 +0800

    Cooper: load data from json to java object.

--- jsonLoader/src/Main.java
+++ jsonLoader/src/Main.java
@@ -9,8 +9,12 @@ public class Main {
     public static void main(String[] args) {
         try {
             String jsonStrings = Files.readString(Path.of("posts.json"));
-            List<Post> posts = JSON.parseArray(jsonStrings,Post.class);
+            List<Post> posts = JSON.parseArray(jsonStrings, Post.class);
             posts.forEach(System.out::println);
+
+            String jsonString = Files.readString(Path.of("replies.json"));
+            List<Replies> replies = JSON.parseArray(jsonString, Replies.class);
+            replies.forEach(System.out::println);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
